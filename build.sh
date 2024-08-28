9328ff8dd78033a454161514b5b5f91bf8c120f1
simba.hu <simba.hu@nio.com>
2024-08-28 09:15:40 +0800

    add env

---
+++ build.sh
@@ -0,0 +1,3 @@
+export PATH=/Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home/bin:$PATH
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home
+./gradlew idea
